8b8583f48fb0727e0ec3d1583c018506f892331d
Witold Hajkowicz <hajko2801@gmail.com>
2022-10-26 22:31:59 +0200

    Communication between classes and objects has been added

--- lesson-1/ex-1.cpp
+++ lesson-1/ex-1.cpp
@@ -3,26 +3,46 @@
 
 using namespace std;
 
+class Person
+{
+    public:
+        int age;
+        string name;
+        string hairColor;
+        
+        Person(int age, string name, string hairColor)
+        {
+            this->age = age;
+            this->name = name;
+            this->hairColor = hairColor;
+        }
+        
+        void Sound()
+        {
+            cout << "Siemano" << endl;
+        }
+};
+
 class Dog
 {
     public:
         int age;
         string name;
         string color;
-        string owner;
+        Person *owner;
     
-    Dog(int _age, string _name, string _color, string _owner)
-    {
-        age = _age;
-        name = _name;
-        color = _color;
-        owner = _owner;
-    }
+        Dog(int age, string name, string color, Person owner)
+        {
+            this->age = age;
+            this->name = name;
+            this->color = color;
+            this->owner = &owner;
+        }
 
-    void Sound()
-    {
-        cout << "Wof" << endl;
-    }
+        void Sound()
+        {
+            cout << "Wof" << endl;
+        }
 
 };
 
@@ -32,54 +52,34 @@ class Cat
         int age;
         string name;
         string color;
-        string owner;
+        Person *owner;
 
-    Cat(int _age, string _name, string _color, string _owner)
-    {
-        age = _age;
-        name = _name;
-        color = _color;
-        owner = _owner;
-    }
-    
-    void Sound()
-    {
-        cout << "Meow" << endl;
-    }
-};
-
-class Person
-{
-    public:
-        int age;
-        string name;
-        string hairColor;
+        Cat(int age, string name, string color, Person owner)
+        {
+            this->age = age;
+            this->name = name;
+            this->color = color;
+            this->owner = &owner;
+        }
         
-    Person(int _age, string _name, string _hairColor)
-    {
-        age = _age;
-        name = _name;
-        hairColor = _hairColor;
-    }
-    
-    void Sound()
-    {
-        cout << "Siemano" << endl;
-    }
+        void Sound()
+        {
+            cout << "Meow" << endl;
+        }
 };
+
 
 
 int main()
 {
     Person person0 = Person(21, "Witek", "Brown");
-    Dog dog0 = Dog(5, "Azor", "Brown", person0.name);
-    Cat cat0 = Cat(10, "Miał", "White", person0.name);
+    Dog dog0 = Dog(5, "Azor", "Brown", person0);
+    Cat cat0 = Cat(10, "Miał", "White", person0);
 
     dog0.Sound();
     cat0.Sound();
-    
-    cout << "Kot"
-    
+    cout << dog0.owner->hairColor << endl;
+    cout << cat0.owner->age << endl;
 
     return 0;
 }

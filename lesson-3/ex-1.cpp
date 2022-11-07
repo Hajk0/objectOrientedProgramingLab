7e984c01398ac5dd61553639e6da53b52ce512e2
Witold Hajkowicz <hajko2801@gmail.com>
2022-11-08 00:53:22 +0100

    Fixed pointers to classes

--- lesson-3/ex-1.cpp
+++ lesson-3/ex-1.cpp
@@ -8,20 +8,21 @@ private:
     string name;
     int age;
     int cash;
+
 public:
-    Person(string name, int age, int cash);
-    void Hello()
+    Person(string name, int age, int cash)
     {
-        cout << "Hello, I'm " << this->name << endl;
+        this->name = name;
+        this->age = age;
+        this->cash = cash;
     }
+    string hello()
+    {
+        return "Hello, I'm " + this->name;
+    }
+
 };
 
-Person::Person(string name, int age, int cash)
-{
-    this->name = name;
-    this->age = age;
-    this->cash = cash;
-}
 
 
 class Brand
@@ -32,15 +33,16 @@ private:
     double stockValue;
 
 public:
-    Brand()
-    {
-        
-    }
     Brand(string name, int yearOfFoundation, double stockValue)
     {
         this->name = name;
         this->yearOfFoundation = yearOfFoundation;
         this->stockValue = stockValue;
+    }
+
+    string brandName()
+    {
+        return name;
     }
 };
 
@@ -48,37 +50,60 @@ public:
 class Vehicle
 {
 private:
-    Brand brand;
+    Brand *brand;
     string name;
     int productionDate;
-    int weight;
     int power;
-    float acceleration;
     float maxSpeed;
-    string owner;
+    long price;
+    Person *owner;
 public:
-    Vehicle(Brand brand, string name, int productionDate, int weight, int power, float acceleration, float maxSpeed, string owner)
+    Vehicle(Brand *brand, string name, int productionDate, int power, float maxSpeed, long price, Person *owner)
     {
         this->brand = brand;
         this->name = name;
         this->productionDate = productionDate;
-        this->weight = weight;
         this->power = power;
-        this->acceleration = acceleration;
         this->maxSpeed = maxSpeed;
+        this->price = price;
         this->owner = owner;
     }    
-    void Buying(string newOwner)
+    
+    string carPrice()
+    {
+        return "The price of " + this->brand->brandName() + " " + this->name + " is: " + to_string(this->price);
+    }
+
+    void changingOwner(Person *newOwner)
     {
         this->owner = newOwner;
-        
+    }
+
+    void info()
+    {
+        cout << "Brand name: " << brand->brandName() << "\nModel name: " << this->name << "\nProduction date: " << this->productionDate << endl;
+        cout << "Horse power: " << this->power << "\nMax speed: " << this->maxSpeed << "\nPrice: " << this->price << endl;
+    }
+
+    void siemano()
+    {
+        cout << this->owner->hello() << ", owner of this car." << endl;
     }
 };
 
 
 int main()
 {
-    
+    Person person0 = Person("Witek", 21, 5);
+    Person person1 = Person("Kuba", 20, 10);
+    Brand brand0 = Brand("Audi", 1976, 20000000);
+    Vehicle vehicle0 = Vehicle(&brand0, "RS5", 2021, 320, 390, 500000, &person0);
+
+    vehicle0.info();
+    cout << vehicle0.carPrice() << endl;
+    vehicle0.changingOwner(&person1);
+    vehicle0.siemano();
+
 
     return 0;
 }
